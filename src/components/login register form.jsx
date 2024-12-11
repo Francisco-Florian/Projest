04957b6c992d89c4edb80cbf1122d2bfafb513d2
Florian Francisco <francisco0.florian@gmail.com>
2024-12-11 10:10:15 +0100

    refine LoginRegisterForm error handling

--- src/components/login register form.jsx
+++ src/components/login register form.jsx
@@ -4,21 +4,20 @@ import PropTypes from 'prop-types';
 export default function LoginRegisterForm({ buttonValue, linkValue, link, forgotPass, onSubmit, fieldErrors }) {
     return (
         <form action="#" method="post" onSubmit={onSubmit}>
-            {fieldErrors.email && <p className="fieldError">{fieldErrors.email}</p>}
+            {fieldErrors?.email && <p className="fieldError">{fieldErrors.email}</p>}
             <label htmlFor="email">Email</label>
             <input type="email" id="email" name="email" placeholder="Enter your email" required />
             
-            {fieldErrors.password && <p className="fieldError">{fieldErrors.password}</p>}
+            {fieldErrors?.password && <p className="fieldError">{fieldErrors.password}</p>}
             <label htmlFor="password">Password</label>
             <input type="password" id="password" name="password" placeholder="Enter your password" required />
-            
+
             <input className="button" type="submit" value={buttonValue} />
-            
             <p>Or</p>
             <Link className="button" to={link}>{linkValue}</Link>
             <Link id="forgotPass" to="/forgot-pass">{forgotPass}</Link>
         </form>
-    )
+    );
 }
 
 LoginRegisterForm.propTypes = {
@@ -27,5 +26,5 @@ LoginRegisterForm.propTypes = {
     link: PropTypes.string.isRequired,
     forgotPass: PropTypes.string,
     onSubmit: PropTypes.func,
-    fieldErrors: PropTypes.object
+    fieldErrors: PropTypes.object,
 };
